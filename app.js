8cd7229f629c89945ed22e72bb29b04f68738b5d
Stafer00 <staffaneriksson@me.com>
2026-03-17 21:11:38 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("APP STARTAR")
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  document.getElementById("saveBtn").addEventListener("click", ()=>{
+    alert("Test – app fungerar")
+  })
+})
+
+function prevWeek(){ alert("Föregående vecka") }
+function nextWeek(){ alert("Nästa vecka") }
