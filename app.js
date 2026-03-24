a9f0168bbc7396faacc579c2ce623869cf4c46ec
Stafer00 <staffaneriksson@me.com>
2026-03-25 00:17:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log("VERSION 1.5 SAFE")
 
 const supabaseClient = window.supabase.createClient(
   "https://ycasdixhobiaiizevgsi.supabase.co",
-  "DIN_FULLA_KEY_HÄR"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljYXNkaXhob2JpYWlpemV2Z3NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNTMxODksImV4cCI6MjA4ODkyOTE4OX0.KtJFN_RhN8WIIPPYX1TfnyZYCdlhug7SBqYnMALOw2c"
 )
 
 let skis=[], rentals=[], filteredRentals=[], cart=[], weekOffset=0
